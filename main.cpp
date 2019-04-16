36e77c80a470413ad34c910a306d3bb360ad28dc
fjsxy <myadfjsxy@163.com>
2019-04-16 16:52:02 +0800

    modify to HikVersion

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include "CameraApi.h"//该头文件会在相机SDK安装后自动出现在/usr/include中
+#include "MvCameraControl.h"
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio/videoio_c.h>
+#include <CameraParams.h>
 
 using namespace std;
 using namespace cv;
 
 struct A{
     Mat frame;
-    int ID;
+    int ID{};
+    string save_path;
 };
 
 void on_mouse(int event, int x,int y,int flags,void* ustc)
@@ -16,9 +18,8 @@ void on_mouse(int event, int x,int y,int flags,void* ustc)
     A* a = static_cast<A*>(ustc);
     if (event == EVENT_LBUTTONDOWN)
     {
-        char buffer[100];
-        sprintf(buffer,"/home/fjsxy/camera_calibration/build/cal_images/image%d.jpg",a->ID);
-        imwrite(buffer,a->frame);
+        string imagePath = a->save_path + "/image" + to_string(a->ID) + ".jpg";
+        imwrite(imagePath.c_str(),a->frame);
         cout << "第" << a->ID+1 << "张图片已保存." << endl;
         a->ID++;
     }
@@ -27,49 +28,159 @@ void on_mouse(int event, int x,int y,int flags,void* ustc)
 
 int main() {
     A a;
-
-    system("rm -rf /home/fjsxy/camera_calibration/build/cal_images/*");
-    int cameraCount = 1;
-    tSdkCameraDevInfo cameraEnumList;
-    int cameraHandle;
-
-    CameraEnumerateDevice(&cameraEnumList,&cameraCount);
-
-    cout << "请输入曝光时间：" << endl;
-    double e_time;
-    cin >> e_time;
     a.ID = 0;
-    tSdkCameraCapbility cameraCapbility;
-    CameraInit(&cameraEnumList,-1,-1,&cameraHandle);
-    CameraGetCapability(cameraHandle,&cameraCapbility);
 
-    if (cameraCapbility.sIspCapacity.bMonoSensor)
-        CameraSetIspOutFormat(cameraHandle, CAMERA_MEDIA_TYPE_MONO8);
-    else
-        CameraSetIspOutFormat(cameraHandle, CAMERA_MEDIA_TYPE_BGR8);
+    float e_time = 20000;
 
-    BYTE* frame_buffer = (unsigned char *)malloc(cameraCapbility.sResolutionRange.iHeightMax *
-                                                 cameraCapbility.sResolutionRange.iWidthMax * 3);
-    CameraSetAeState(cameraHandle, false);
-    CameraSetExposureTime(cameraHandle,e_time);
-    CameraPlay(cameraHandle);
+    cout << "Please input exposure time:" << endl;
+    cin >> e_time;
 
-    int cvtype = CV_8UC1;
-    BYTE* tmp_buffer;
-    while(true) {
-        tSdkFrameHead frameInfo;
-        CameraGetImageBuffer(cameraHandle, &frameInfo, &tmp_buffer, 2000);
-        CameraImageProcess(cameraHandle, tmp_buffer, frame_buffer, &frameInfo);
-        CameraReleaseImageBuffer(cameraHandle, tmp_buffer);
-        if (frameInfo.uiMediaType & CAMERA_MEDIA_TYPE_RGB)
-            cvtype = CV_8UC3;
-        a.frame = Mat(frameInfo.iHeight, frameInfo.iWidth, cvtype, frame_buffer);
+    a.save_path = "/home/fjsxy/catkin_ws/src/VINS-Fusion/camera_models/camera_calib_example/cal_image";
+//    cout << "Please input the path to save pictures(you should ensure your path is correct):" << endl;
+//    cin >> a.save_path;
 
-        namedWindow("test");
-        setMouseCallback("test", on_mouse,&a);
-        imshow("test", a.frame);
-        if (waitKey(30) > 0)
-            return 0;
+    cout << "Do you want to delete all files in path[" << a.save_path << "], y for YES, n for NO[y or n]:" ;
+    string checkByte;
+    cin >> checkByte;
+    if (checkByte == "y")
+    {
+        cout << "Begin to delete the old files..." << endl;
+        string rm_command = "rm -rf " + a.save_path + "/*"; // rm -rf your_path/*
+        system(rm_command.c_str());
+        cout << "Delete complete!" << endl;
+    }
+    else if (checkByte == "n")
+    {
+        cout << "Don't delete the old files..." << endl;
+        cout << "continue..." << endl;
+    }
+
+
+    // 枚举设备
+    int nRet = MV_OK;
+    MV_CC_DEVICE_INFO_LIST deviceInfoList;
+    nRet = MV_CC_EnumDevices(MV_USB_DEVICE | MV_GIGE_DEVICE,&deviceInfoList);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera Enum Error! " << nRet << endl;
+        return -1;
+    }
+
+    // 打印枚举设备信息
+    cout << endl << "Device List:" << endl;
+    for (int i = 0; i < deviceInfoList.nDeviceNum; ++i) {
+        cout << "[device " << i << "]:" << endl;
+        if (deviceInfoList.pDeviceInfo[i]->nTLayerType == MV_USB_DEVICE)
+            cout << deviceInfoList.pDeviceInfo[i]->SpecialInfo.stUsb3VInfo.chModelName << endl;
+        else if (deviceInfoList.pDeviceInfo[i]->nTLayerType == MV_GIGE_DEVICE)
+            cout << deviceInfoList.pDeviceInfo[i]->SpecialInfo.stGigEInfo.chModelName << endl;
+        else
+            cout << "Not Support." << endl;
+    }
+
+    // 选择设备
+    int chooseID = 0;
+    cout << "Please choose one cameraID you want:" << endl;
+    while(cin >> chooseID)
+    {
+        if (chooseID >= deviceInfoList.nDeviceNum)
+        {
+            cout << "Input error. Please input ID again:" << endl;
+        }
+        else
+            break;
+    }
+
+    // 创建设备句柄
+    void *cameraHandle = nullptr;
+    nRet = MV_CC_CreateHandle(&cameraHandle,deviceInfoList.pDeviceInfo[chooseID]);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera CreateHandle Error! " << nRet << endl;
+        return -1;
+    }
+
+    // 打开设备
+    nRet = MV_CC_OpenDevice(cameraHandle);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera OpenDevice Error! " << nRet << endl;
+        return -1;
+    }
+
+    // 设置基本参数（目前只设置了曝光时间，可根据需要添加）
+    nRet = MV_CC_SetExposureTime(cameraHandle,e_time);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera SetParameter Error! " << nRet << endl;
+        return -1;
+    }
+
+    // 开始抓图
+    nRet = MV_CC_StartGrabbing(cameraHandle);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera StartGrabbing Error! " << nRet << endl;
+        return -1;
+    }
+
+    // 获取图像参数以及大循环
+    MVCC_INTVALUE intParam;
+    memset(&intParam, 0, sizeof(MVCC_INTVALUE));
+    nRet = MV_CC_GetIntValue(cameraHandle,"PayloadSize",&intParam); //获取int型中名为PayloadSize的参数
+    if (nRet != MV_OK)
+    {
+        cout << "Camera Get PayloadSize Error! " << nRet << endl;
+        return -1;
+    }
+    MV_FRAME_OUT_INFO_EX frameInfo = {0};
+    memset(&frameInfo, 0 , sizeof(MV_FRAME_OUT_INFO_EX));
+    auto* imageData = (unsigned char *)malloc(sizeof(unsigned char) * intParam.nCurValue);
+    if (!imageData)
+    {
+        cout << "malloc failed!" << endl;
+        return -1;
+    }
+    int dataSize = intParam.nCurValue;
+    while(true)
+    {
+        nRet = MV_CC_GetOneFrameTimeout(cameraHandle,imageData,dataSize,&frameInfo,2000);
+        if (nRet == MV_OK) {
+            a.frame = Mat(frameInfo.nHeight,frameInfo.nWidth,CV_8UC1,imageData);
+            namedWindow("show");
+            setMouseCallback("show", on_mouse,&a);
+            imshow("show", a.frame);
+            if (waitKey(30) > 0)
+                break;
+        }
+        else {
+            cout << "NO image data!" << endl;
+        }
+    }
+
+    // 释放存储图像内存空间
+    cout << "free..." << endl;
+    free(imageData);
+
+    // 停止阶段
+    cout << "stop..." << endl;
+    nRet = MV_CC_StopGrabbing(cameraHandle);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera StopGrabbing Error! " << nRet << endl;
+        return -1;
+    }
+    nRet = MV_CC_CloseDevice(cameraHandle);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera CloseDevice Error! " << nRet << endl;
+        return -1;
+    }
+    nRet = MV_CC_DestroyHandle(cameraHandle);
+    if (nRet != MV_OK)
+    {
+        cout << "Camera DestroyHandle Error! " << nRet << endl;
+        return -1;
     }
 
     return 0;
